c942ac819ced5ee4f1a752c14aedca4fd2e95138
coder-akash <>
2023-01-01 22:47:18 -0800

    Task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_ch = ''
+    for i in str:
+        if ord(i) > 96:
+            upper_ch = upper_ch + chr(ord(i)-32)
+        else:
+            upper_ch += i
+    print(upper_ch)

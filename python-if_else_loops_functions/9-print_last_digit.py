e2351070a07d299cd66dda88be158a746c60cf81
coder-akash <>
2023-01-01 23:17:30 -0800

    Task 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number >= 0:
-        return number % 10
-    else:
-        return (number % 10) - 10
+    lst_no = number % 10
+    if number < 0:
+        lst_no = lst_no - 10
+    return lst_no

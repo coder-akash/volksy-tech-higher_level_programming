ecb050d86c7d673d4ee73a3069ff9da890a60b3b
coder-akash <>
2023-01-03 21:59:11 -0800

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from sys import argv
-    print('{} argument:'.format(len(argv)-1))
+    print('{} arguments:'.format(len(argv)-1))
     for i in range(1, len(argv)):
         print('{}: {}'.format(i, argv[i]))

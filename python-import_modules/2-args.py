8b74c3e1ec31118100dd04c533319755af5b9ac4
coder-akash <>
2023-01-03 23:33:57 -0800

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     if len(argv) - 1 == 0:
         print('{} arguments.'.format(len(argv)-1))
     elif len(argv) - 1 == 1:
-        print('{} arguments:'.format(len(argv)-1))
+        print('{} argument:'.format(len(argv)-1))
     else:
-        print('{} arguments:'.format(len(argv)-1))        
+        print('{} arguments:'.format(len(argv)-1))
     for i in range(1, len(argv)):
         print('{}: {}'.format(i, argv[i]))

63469361bcd95173e2c2844b64f1255f471bfc76
coder-akash <>
2023-01-04 03:07:12 -0800

    Task6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    from calculator_1 import add , sub, mul, div
+    from calculator_1 import add, sub, mul, div
     from sys import argv, exit
     args = argv[1:]
     if len(args) == 3:
-        a, b ,c = args
+        a, b, c = args
         if b == '+':
             res = add(int(a), int(c))
         elif b == '-':
             res = sub(int(a), int(c))
         elif b == '*':
-            res = add(int(a), int(c))
+            res = mul(int(a), int(c))
         elif b == '/':
-            res = add(int(a), int(c))
+            res = div(int(a), int(c))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)

6a5725eca0f9ea0233cb31b87ae9dc5b8d0692d0
coder-akash <>
2023-01-05 04:07:28 -0800

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,16 +2,15 @@
 def add_tuple(tup1,tup2):
     if len(tup1) > 2:
         tup1 = tup1[:2]
-    elif len(tup2) > 2:
-        tup2 = tup2[:2]
     elif len(tup1) == 1:
         tup1 = tuple((tup1[0], 0))
+    else:
+        tup1 = tuple((0, 0))
+    if len(tup2) > 2:
+        tup2 = tup2[:2]
     elif len(tup2) == 1:
         tup2 = tuple((tup2[0], 0))
-    elif len(tup1) == 0:
-        tup1 = tuple((0, 0))
-    elif len(tup2) == 0:
+    else:
         tup2 = tuple((0, 0))
-    a, b = tup1
     c, d = tup2
     return tuple((a + c, b + d))

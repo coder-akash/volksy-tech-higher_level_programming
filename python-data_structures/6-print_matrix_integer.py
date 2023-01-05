f4b8c1eb3b15068a8b69428d0f7d14348d69346d
coder-akash <>
2023-01-05 03:03:18 -0800

    Task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(mat):
     for i in range(len(mat)):
         for j in mat[i]:
-            print('{:d}'.format(j), end=" ")
-        print()
+            if j == mat[i][-1]:
+                print(j)
+            else:
+                print('{:d}'.format(j), end=" ")

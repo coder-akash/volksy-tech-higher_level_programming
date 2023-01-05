2861d57fe6807996bdb3a5f605dace70e03008f0
coder-akash <>
2023-01-05 01:45:09 -0800

    Task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,8 +3,8 @@ def new_in_list(my_list, idx, new_element):
     copy = my_list.copy()
     if idx < 0:
         return copy
-    elif idx > len(my_list) -1:
+    elif idx > len(my_list) - 1:
         return my_list
     else:
-        my_list[idx] = new_element
-        return my_list
+        copy[idx] = new_element
+        return copy

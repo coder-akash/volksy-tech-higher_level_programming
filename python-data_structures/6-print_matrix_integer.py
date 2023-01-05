113a8e24c6b8e20f613c58e3ea1336bb510c5ad2
coder-akash <>
2023-01-05 03:13:41 -0800

    Task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(mat):
     for i in range(len(mat)):
+        if len(mat[i]) == 0:
+            print()
         for j in mat[i]:
             if j == mat[i][-1]:
                 print(j)

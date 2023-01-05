860317392f40d520ebbe6aaba31282af4209a3a2
coder-akash <>
2023-01-05 08:29:29 -0800

    Task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list, idx):
-    if idx < 0 or idx > len(lst):
+    if idx < 0 or idx > len(my_list):
         return my_list
     else:
         lst = my_list[:idx] + my_list[idx+1:]

b131f0a653898d11951d9c4e3cd25667e85faada
coder-akash <>
2023-01-06 08:02:10 -0800

    Task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def roman_to_int(st):    
-    d1 = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+def roman_to_int(st):
+    d1 = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
     if type(st) == str:
         for i in range(len(st)):

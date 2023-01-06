5a08c1ceadd0710b5745a506275175b2137134ab
coder-akash <>
2023-01-06 09:16:43 -0800

    task 15

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(d, ele):
-    new_dic = {}
     if ele in d.values():
-        for i, j in d.items():
-            if j != ele:
-                new_dic[i] = j
-        return new_dic
+        for i in list(d.keys()):
+            if d[i] == ele:
+                d.pop(i)
+        return d
     else:
         return d

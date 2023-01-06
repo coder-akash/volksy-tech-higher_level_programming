b7ec628f374eae271d04e8dae68309a6b56fe205
coder-akash <>
2023-01-06 10:38:14 -0800

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,9 @@
 def safe_print_list_integers(l, x):
     try:
         c = 0
-        l.index(x)
-        for i in l[:x]:
-            if type(i) == int:
-                print('{:d}'.format(i), end="")
+        for i in range(x):
+            if type(l[i]) == int:
+                print('{:d}'.format(l[i]), end="")
                 c += 1
         print()
         return c

f2daad037b3c87a7391d14fbf3d80bbbf8504e4f
coder-akash <>
2023-01-09 01:27:25 -0800

    task 7

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(val):
     try:
         print('{:d}'.format(val))
         return True
     except ValueError as err:
-        print('Exception: ', err)
+        sys.stderr.write('Exception: ', err)
         return False
     except TypeError as err:
-        print('Exception: ', err)
+        sys.stderr.write('Exception: ', err)
         return False

90015bba1187101e5ead0ace757ed0aa1a6558b5
coder-akash <>
2023-01-11 04:22:27 -0800

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,12 +5,9 @@
 class Square:
     ''' define private attribute with exception handling'''
     def __init__(self, s=0):
-        try:
-            if type(s) != int:
-                raise TypeError('size must be an integer')
-            elif s < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = s
-        finally:
-            pass
+        if type(s) != int:
+            raise TypeError('size must be an integer')
+        elif s < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = s

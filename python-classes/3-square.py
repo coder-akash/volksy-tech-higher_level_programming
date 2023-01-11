732413c8c183870bde1df3a37a332b36c4585902
coder-akash <>
2023-01-11 01:37:54 -0800

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' defining class '''
+
+
+class Square:
+    ''' define private attribute with exception handling and returning area of square'''
+    def __init__(self, s=0):
+        try:
+            if type(s) != int:
+                raise TypeError('size must be an integer')
+            elif s < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = s
+        finally:
+            pass
+    def area(self):
+        return self.__size**2

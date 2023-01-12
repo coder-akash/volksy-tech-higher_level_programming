bc4234e2ec8f4d3eb463f84b8a2015104978ae7e
coder-akash <>
2023-01-11 20:22:02 -0800

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -52,12 +52,7 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                if self.__position[0] > 0 and self.__position[1] > 0:
-                    print(' ' * self.__position[0] + '#' * self.__size + 
-                            ' ' * self.__position[1])
-                elif self.__position[0] > 0:
+                if self.__position[0] > 0:
                     print(' ' * self.__position[0] + '#' * self.__size)
-                elif self.__position[1] > 0:
-                    print('#' * self.__size + ' '*self.__position[1])
                 else:
                     print('#' * self.__size)

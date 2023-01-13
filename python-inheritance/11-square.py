7df21feed6fedaf17222a4fb5e075ad9fb025ccc
coder-akash <>
2023-01-13 04:38:33 -0800

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' task 11 '''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    ''' class '''
+
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return ('[Square] {}/{}'.format(self.__size, self.__size))

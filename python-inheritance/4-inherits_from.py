6fd69796402231a6345696e4cf4dac2ab2bc0f9a
coder-akash <>
2023-01-13 01:32:01 -0800

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' task 4 '''
+
+
+def def inherits_from(obj, a_class):
+    ''' Function '''
+    
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False

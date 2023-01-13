76be1adbad1275f7f9d21a5b44d4e5b6e071b072
coder-akash <>
2023-01-13 01:38:31 -0800

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,8 +4,8 @@
 
 def inherits_from(obj, a_class):
     ''' Function '''
-    
-    if isinstance(obj, a_class):
+
+    if isinstance(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False

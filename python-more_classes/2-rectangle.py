e86034b1a9acd49716615aa7c73336527d3e9653
coder-akash <>
2023-01-16 02:15:47 -0800

    task 2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         return self.__width

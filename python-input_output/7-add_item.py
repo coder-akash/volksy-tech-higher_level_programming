b9e11e27cd8766c63f6d39a066bf3edae8ba7cb2
coder-akash <>
2023-01-18 04:36:33 -0800

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,13 +2,14 @@
 ''' task 7 '''
 import sys
 
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        vals = load_from_json_file("add_item.json") 
+        vals = load_from_json_file("add_item.json")
     except FileNotFoundError:
         vals = []
     vals += sys.argv[1:]

92b5389590b231b3da6309407727b6205fb9793b
coder-akash <>
2023-01-18 04:13:52 -0800

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,9 @@ import sys
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-    with open('add_item.json', 'w') as fp:
-        lst = list(load_from_json_file(fp))
-        i = sys.argv[1:]
-        lst += i
-        save_to_json_file(lst, fp)
+    with open('add_item.json','w') as fp:
+        save_to_json_file([], "add_item.json")
+        vals = load_from_json_file("add_item.json")
+        for i in sys.argv[1:]:
+            vals.append(i)
+        save_to_json_file(items, "add_item.json")

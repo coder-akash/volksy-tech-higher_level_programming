7a17be74f1f1f1d8a8ec3306d122a40e5dd65b98
coder-akash <>
2023-01-18 03:31:41 -0800

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ import sys
 
 
 with open('add_item.json', w) as fp:
-    lst = list(load_from_json_file(fp))
+    '''lst = list(load_from_json_file(fp))'''
     i = sys.argv[1:]
     lst += i
     save_to_json_file(lst, fp)

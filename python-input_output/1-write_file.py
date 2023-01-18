5df046ba6cf8be16ca2f9a57e4b15825abd77907
coder-akash <>
2023-01-18 02:32:39 -0800

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 ''' task 1 '''
 
 
-def def write_file(filename="", text=""):
+def write_file(filename="", text=""):
     ''' function '''
     with open(filename, 'w', encoding='utf_8') as fp:
         fp.write(text)

6cacb3bd8a704372ffda28461720b9fddeab738b
coder-akash <>
2023-01-18 03:27:52 -0800

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-''' task 7 '''
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
+''' task 7 '''
 
 
 with open('add_item.json', w) as fp:

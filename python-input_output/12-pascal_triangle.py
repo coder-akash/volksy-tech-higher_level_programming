8cbb30d77ce72bb96e23445c9f30b178f6e62acb
coder-akash <>
2023-01-19 04:28:13 -0800

    task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -8,7 +8,11 @@ def pascal_triangle(n):
         return []
     else:
         lst = []
-        for i in range(n):
-            l = [int(j) for j in str(11**i)]
+        for i in range(1, n+1):
+            C = 1
+            l = []
+            for j in range(1, i+1):
+                l.append(C)
+                C = C * (i - j) // j
             lst.append(l)
         return lst

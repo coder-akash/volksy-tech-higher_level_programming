e5f5e888a009aef6292b82ca381649772cef2fc9
coder-akash <>
2023-01-19 04:37:43 -0800

    task 13

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' task 13 '''
+
+def append_after(filename="", search_string="", new_string=""):
+    ''' function '''
+    with open(filename, encoding='utf_8') as fp:
+        x = fp.read()
+        new = x.remove(search_string, new_string)
+        with open(filename, 'w', encoding='utf_8') as fp1:
+            fp1.write(new)

5f8bcc6f8c5260753a9135612812c8f0d74af07c
coder-akash <>
2023-01-20 10:34:19 -0800

    task square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,6 +8,15 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self,val):
+        self.width = val
+        self.height = val
+
     def __str__(self):
         return ('[Square] ({}) {}/{} - {}'.format
                 (self.id, self.x, self.y, self.width))

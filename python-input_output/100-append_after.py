38d08f290de61b349fc98691ed7777d1aa21c352
coder-akash <>
2023-01-19 20:38:40 -0800

    task 13

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -7,7 +7,9 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, encoding='utf_8') as fp:
         x = fp.readlines()
         for i in x:
-            if search_string in i:
-                x.insert(x.index(i)+1, new_string)
+            if search_string in i and '\n' in i:
+                print(i)
+            else:
+                x.insert(x.index(i)+1, '\n'+new_string)
         with open(filename, 'w', encoding='utf_8') as fp1:
             fp1.writelines(x)

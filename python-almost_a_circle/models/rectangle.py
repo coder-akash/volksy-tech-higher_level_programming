dadfb8d77ea4df0f5c5a68095c9ba179f6c1e5b3
coder-akash <>
2023-01-19 22:31:23 -0800

    task 1

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" task 2 """
+
+
+class Rectangle(Base):
+    ''' class '''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        __width = width
+        __height = height
+        __x = x
+        __y = y

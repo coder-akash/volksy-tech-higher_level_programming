c7c1fedd5b63d6815ec73115212f2eb13a2e98e7
coder-akash <>
2023-01-19 22:47:34 -0800

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,8 +7,8 @@ class Rectangle(Base):
     ''' class '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        __width = width
-        __height = height
-        __x = x
-        __y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         Base.__init__(id)

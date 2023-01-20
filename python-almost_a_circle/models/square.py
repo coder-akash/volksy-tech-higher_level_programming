b11b1390bd197b107f7a0ab8a543e23f0caa222c
coder-akash <>
2023-01-20 04:59:01 -0800

    task square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,6 +4,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    ''' class '''
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 

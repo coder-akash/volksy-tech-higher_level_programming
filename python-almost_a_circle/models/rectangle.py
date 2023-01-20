6701a0ffef1c8178982f113f71d7b4febf5c3c6d
coder-akash <>
2023-01-19 23:22:18 -0800

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,10 +5,42 @@ from models.base import Base
 
 class Rectangle(Base):
     ''' class '''
-
+    
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
+    
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self ,val):
+        self.__width = val
+
+    @height.setter
+    def height(self, val):
+        self.__height = val
+
+    @x.setter
+    def x(self, val):
+        self.__x = val
+
+    @y.setter
+    def y(self, val):
+        self.__y = val

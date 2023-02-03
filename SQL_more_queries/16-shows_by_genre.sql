900ad97ac0214babcbeb2164ac01752eed90f611
coder-akash <>
2023-02-02 21:12:57 -0800

    task 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
 SELECT title, name
 FROM tv_shows
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY name, title;

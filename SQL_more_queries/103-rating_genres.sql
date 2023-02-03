1c8d902dfb92e5e47717ec79c641ad518143b531
coder-akash <>
2023-02-03 02:20:07 -0800

    task 20

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,9 @@
--- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- script that lists all genres in the database hbtn_0d_tvshows_rate by their ratings.
 SELECT name, SUM(rate) AS rating
 FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_show_ratings
-ON tv_genres.id = tv_show_ratings.show_id
+ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY name
 ORDER BY rating DESC;

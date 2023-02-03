ce00ee5f3c2be1bbb94b41641c3398f95b9d7230
coder-akash <>
2023-02-02 20:37:40 -0800

    task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,5 @@ INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id 
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id 
-WHERE title = 'Dexter';
+WHERE title = 'Dexter'
+ORDER BY name;

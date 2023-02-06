02012b52c226a4f3613d5f1f055d78475b7605ad
coder-akash <>
2023-02-06 03:17:56 -0800

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-from sqlalchemy import Column, Interger, String
-from sqlalchemy.orm import declarative_base
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class State(Base):
-    ''' class '''
 
-    __tablename__ = 'states'
-    id = Column(Interger, primary_key=True)
+class State(Base):
+    """ class """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)

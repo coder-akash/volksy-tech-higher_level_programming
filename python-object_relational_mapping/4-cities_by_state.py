5d9cfd5b507b1e8022de8340ee480f055ed26277
coder-akash <>
2023-02-05 23:09:55 -0800

    task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     conn = mysql.connect(user=sys.argv[1], passwd=sys.argv[2],
                          database=sys.argv[3])
     cur = conn.cursor()
-    cur.execute('SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY id')
+    cur.execute('SELECT cities.id, cities.name, states.name FROM cities
+            INNER JOIN states ON cities.state_id = states.id ORDER BY id')
     data = cur.fetchall()
     for i in data:
         print(i)

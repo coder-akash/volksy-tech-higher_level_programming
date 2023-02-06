0adf154c0a24a62f3de1bf4b3bc0aa7435342687
coder-akash <>
2023-02-05 20:10:06 -0800

    task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import MySQLdb as mysql
+conn = mysql.connect(host='localhost', user='root',passwd='',database='hbtn_0e_0_usa')
+cur = conn.cursor()
+cur.execute('SELECT * FROM states ORDER BY id')
+data =  cur.fetchall()
+for i in data:
+    print(i)

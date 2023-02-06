13c6224da79b00691441d2a8f8d04f41794388e5
coder-akash <>
2023-02-05 22:13:33 -0800

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     conn = mysql.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     names = sys.argv[4:]
     cur = conn.cursor()
-    cur.execute('SELECT * FROM states WHERE name IN {} ORDER BY id'.format(tuple(names))
+    cur.execute('SELECT * FROM states WHERE name IN {} ORDER BY id'.format(tuple(names)))
     data = cur.fetchall()
     for i in data:
         print(i)

7430217c8c33e216ee99ea9383558886e62149d7
coder-akash <>
2023-02-05 21:12:51 -0800

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ import MySQLdb as mysql
 
 if __name__ == "__main__":
     conn = mysql.connect(host='localhost', user='root', passwd='root',
-                         database='hbtn_0e_0_usa')
+                         database=hbtn_0e_0_usa)
     cur = conn.cursor()
     cur.execute('SELECT * FROM states WHERE name LIKE N% ORDER BY id')
     data =  cur.fetchall()

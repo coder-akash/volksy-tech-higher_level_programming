209b20318f6bf71b3017c14dfd39661e5c63f698
coder-akash <>
2023-02-05 21:19:14 -0800

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     conn = mysql.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
     cur.execute('SELECT * FROM states WHERE name LIKE N% ORDER BY id')
-    data =  cur.fetchall()
+    data = cur.fetchall()
     for i in data:
         print(i)

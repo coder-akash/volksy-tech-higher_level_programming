501af876aac48bbf63b216953f7d002cbd8e61da
coder-akash <>
2023-02-06 01:45:13 -0800

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     cur.execute('''select cities.name from cities inner join states
     on cities.state_id = states.id where states.name = '{}' '''.format(name))
     data = cur.fetchall()
-    for i in range(len(data)):
-        if i == len(data) - 1:
-            print(data[i][0])
-        else:
-            print(data[i][0], end=', ')
+    if len(data) > 0:
+        for i in range(len(data)):
+            if i == len(data) - 1:
+                print(data[i][0])
+            else:
+                print(data[i][0], end=', ')

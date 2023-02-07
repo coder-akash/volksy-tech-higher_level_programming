621537d09cabfa2af1195f3c0f5d621dcb79426d
coder-akash <>
2023-02-06 20:50:01 -0800

    task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ task 7 """
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from model_state import States
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State

8fcd4a1fd6aa199e70732f664a2c84f6ac463aa6
coder-akash <>
2023-02-08 21:44:52 -0800

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,5 +11,7 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     session = sessionmaker(bind=engine)
     s = session()
+    '''
     data = s.query(State).order_by(State.id).one()
     print('{}: {}'.format(data.id data.name))
+    '''

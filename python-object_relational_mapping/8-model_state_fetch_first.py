5e18f0462b1f3ca85c2d55d88e71f07739601d53
coder-akash <>
2023-02-10 02:36:07 -0800

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,21 +10,16 @@ if __name__ == "__main__":
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
-    # Initialize engine
     Base.metadata.create_all(engine)
 
-    # Initialize session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query
     query = session.query(State).first()
 
-    # Print query
     if query is None:
         print("Nothing")
     else:
-        print("{}: {}".format(query.id, query.name))
+        print("{}: {}".format(query.__dict['id']__, query.__['name']__))
 
-    # Close session
     session.close()

9f42b726db8362f1b79b51697534fb8c49c0e72c
coder-akash <>
2023-02-10 02:58:44 -0800

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""List first State objects from db"""
+"""Task 8"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from model_state import Base, State
 
 
-def list_first_state_obj():
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    s = Session()
 
-    f = session.query(State).first()
+    data = s.query(State).order_by(State.id).first()
 
-    if f:
-        print("{}: {}".format(f.__dict__['id'], f.__dict__['name']))
-    else:
+    if data is None:
         print("Nothing")
-
-    session.close()
-
-
-if __name__ == "__main__":
-    list_first_state_obj()
+    else:
+        print("{}: {}".format(data.id, data.name))
 

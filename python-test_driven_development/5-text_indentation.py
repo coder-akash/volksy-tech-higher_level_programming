3407b1cb51b75d87486863f8fb7606bb047ab204
coder-akash <>
2023-02-13 02:00:41 -0800

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-''' Write a function that prints a text with 2 new lines after each of these characters: ., ? and : '''
+''' Write a function that prints a text with 2 new lines after each of
+these characters: ., ? and : '''
 
 
 def text_indentation(text):
     '''text_indentation'''
     l = len(text)
-    c=0
-    while c!=l:
+    c = 0
+    while c != l:
         if text[c] in '.:?' or text[c] == '\n':
-            print(text[c], end='')
-            print('\n')
-            c+=2
+            if text[c+1] != ' ':
+                print(text[c], end='')
+                print('\n')
+                c+=1
+            else:
+                print(text[c+1], end='')
+                print('\n')
+                c+=2
         else:
             print(text[c], end='')
             c+=1

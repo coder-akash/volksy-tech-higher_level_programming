ef48c72cc4285b489b31b9bf144d16ee5c6760c1
coder-akash <>
2023-02-12 21:39:19 -0800

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@
 
 def add_integer(a, b=98):
     ''' This function returns the addition of a, b '''
-    
+
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:

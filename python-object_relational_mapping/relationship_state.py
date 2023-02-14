d81453ef79850cbe58958a0df714ff2bcf2223ed
coder-akash <>
2023-02-14 02:34:34 -0800

    task 15

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ task 15 """
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from relationship_city import Base, City
 
 
 class State(Base):

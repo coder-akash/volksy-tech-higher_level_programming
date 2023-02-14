040dc7061c2f728976ea4e3e97610a30d9d50c4a
coder-akash <>
2023-02-13 21:14:25 -0800

    task 11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
-    
     Session = sessionmaker(bind=engine)
     session = Session()
 

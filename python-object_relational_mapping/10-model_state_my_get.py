148acfbcfe315f4dcc3bc17de1a65729ff176942
coder-akash <>
2023-02-13 20:25:33 -0800

    task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Task 10"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    
+    session = sessionmaker(bind=engine)
+    s = session()
+    name = sys.argv[4]
+    
+    data = s.query(State).filter_by(State.name = name).one()
+    if data is None:
+        print('Not found')
+    else:
+        print(data.id)
+
+    s.close()

2f8d437549ce5926ec72eaa200f785158b758012
coder-akash <>
2023-02-14 01:56:29 -0800

    task 14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Task 14"""
+import sys
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    q = session.query(State, City).filter(State.id == City.state_id).all()
+    for s, c in query:
+        print("{}: ({}) {}".format(s.name, c.id, c.name))
---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Task 14"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False,
+                      foreign_keys='states.id')

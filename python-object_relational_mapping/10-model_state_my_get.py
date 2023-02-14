43c395a21154b9790326e19ffd5d65e665b99cac
coder-akash <>
2023-02-13 20:34:25 -0800

    task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -7,8 +7,9 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
+    '''
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)'''
 
     Base.metadata.create_all(engine)
 

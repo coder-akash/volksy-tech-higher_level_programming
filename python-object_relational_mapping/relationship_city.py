5145526ede3be6ba2d97488af415d7ba66928418
coder-akash <>
2023-02-14 02:29:42 -0800

    task 15

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Task 15"""
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Table, Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -11,8 +9,6 @@ Base = declarative_base()
 class City(Base):
     ''' cities class '''
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False,
-                unique=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False,
-                      foreign_keys='states.id')
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)

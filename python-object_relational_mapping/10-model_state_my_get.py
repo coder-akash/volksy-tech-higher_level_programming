0ec9edfb8b681b42e59df7809f6de46b3069a16c
coder-akash <>
2023-02-13 20:33:34 -0800

    task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Task 10"""
+""" Task 10"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -7,12 +7,13 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
-    session = sessionmaker(bind=engine)
-    s = session()
+    Session = sessionmaker(bind=engine)
+    s = Session()
     name = sys.argv[4]
 
     data = s.query(State).filter_by(State.name = name).one()

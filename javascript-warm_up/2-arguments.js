4fb4c9fc4d19908dfa70e66817204c90c51a85ea
coder-akash <akashd@brightcomgroup.com>
2023-02-20 20:56:44 -0800

    task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const x = process.argv.length;
-console.log(x == 3 ? 'Argument found' : x > 3 ? 'Arguments found' : 'No argument');
+console.log(x === 3 ? 'Argument found' : x > 3 ? 'Arguments found' : 'No argument');

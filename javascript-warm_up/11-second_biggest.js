3602f4ee8e8c1a722ac3b3e049e20b8212aa4237
coder-akash <akashd@brightcomgroup.com>
2023-02-20 23:09:23 -0800

    task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-let x = process.argv.slice(2)
-    //console.log(x)
+const x = process.argv.slice(2);
+// console.log(x)
 if (x.length === 0 || (x.length === 1 && x[0] === '1')) {
-    console.log(0)
+  console.log(0);
 } else {
-    x.sort().reverse()
-    console.log(x[1])
+  console.log(Math.max.apply(null, x));
 }

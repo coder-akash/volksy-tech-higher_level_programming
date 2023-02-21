a2b804ba338f28549f35c826d45f39e28ea74ee1
coder-akash <akashd@brightcomgroup.com>
2023-02-20 21:30:25 -0800

    task 7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const x = Number.parseInt(process.argv[2]);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else if (x > 0) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}

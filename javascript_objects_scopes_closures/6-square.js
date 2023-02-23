061f7d60ae38d7a93bea7d1d18d5e7fa7cbc1e7f
coder-akash <akashd@brightcomgroup.com>
2023-02-22 20:47:48 -0800

    task 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Square extends require('./demo.js') {
+  constructor (s) {
+    super(s, s);
+  }
+
+  charPrint (c) {
+    if (typeof c !== 'undefined') {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      super.print();
+    }
+  }
+}
+module.exports = Square;
